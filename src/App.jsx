e1ed2a3fdfe8c465d20af0f4b8ec86d9aafd41a8
ArdiansCode <ardiansyah6246@gmail.com>
2026-03-25 20:56:51 +0700

    add link to home

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs" class="color-green-800">
-        Click on the Vite and React logos to learn more
-      </p>
+    <a href="/Home" className='bg-gray-400 px-4 py-2 rounded flex justify-center items-center m-20 text-white text-base'>Klik Untuk ke Halaman E-commerce</a>
     </>
   )
 }
